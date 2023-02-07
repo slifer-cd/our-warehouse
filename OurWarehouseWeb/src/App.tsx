ca0984f7ef195f4692ac942db5c408231a494df7
Ahmad herzallah <ahmad.herz.cd@gmail.com>
2023-02-07 08:54:53 +0200

    header changed

--- OurWarehouseWeb/src/App.tsx
+++ OurWarehouseWeb/src/App.tsx
@@ -2,14 +2,17 @@ import { FC } from "react";
 import { Header } from "./components/";
 import { NotFound } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import "./sass/mian.scss";
 const App: FC = (): JSX.Element => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div style={{ marginTop: 70 }}>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
   );
 };
--- OurWarehouseWeb/src/components/Header.tsx
+++ OurWarehouseWeb/src/components/Header.tsx
@@ -1,12 +1,24 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import "../sass/header.scss";
 import { Link } from "react-router-dom";
 const Header: FC = (): JSX.Element => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [pressed, setPressed] = useState(false);
+  useEffect((): (() => void) => {
+    const handelResize = (): void => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handelResize);
+    return () => {
+      window.removeEventListener("resize", handelResize);
+    };
+  }, []);
+
   return (
-    <header className="overflow-hidden bg-light">
+    <header className="bg-light w-100">
       <Container className="d-flex justify-content-between align-items-center h-100">
-        <div className="h-100 d-flex align-items-center">
+        <div className="h-100 d-flex align-items-center logo">
           <img
             src="/welcomeLogo.png"
             alt="our warehouse logo"
@@ -16,17 +28,45 @@ const Header: FC = (): JSX.Element => {
             our warehouse
           </Link>
         </div>
-        <ul className="d-flex align-items-center h-100">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Storages">Storages</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-        </ul>
+        {windowWidth > 720 ? (
+          <ul className="d-flex align-items-center h-100 non">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Storages">Storages</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+          </ul>
+        ) : (
+          <>
+            <div
+              className="spans-container position-relative h-100"
+              onClick={(e): void => {
+                setPressed((pre) => !pre);
+                e.currentTarget.classList.toggle("pressed");
+                document.querySelector(".is")?.classList.toggle("pressed");
+              }}
+            >
+              <span className="span-1 bg-secondary w-50 d-inline-block"></span>
+              <span className="span-2 bg-secondary w-50 d-inline-block"></span>
+              <span className="span-3 bg-secondary w-50 d-inline-block"></span>
+            </div>
+            <ul className="position-absolute is w-100 bg-light d-flex flex-column">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Storages">Storages</Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+            </ul>
+          </>
+        )}
       </Container>
     </header>
   );
