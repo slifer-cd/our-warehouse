c932746b3d4f3d5565dcc63107d774d9745f98c8
Ahmad herzallah <ahmad.herz.cd@gmail.com>
2023-02-08 17:00:36 +0200

    m

--- OurWarehouseWeb/src/App.tsx
+++ OurWarehouseWeb/src/App.tsx
@@ -1,10 +1,25 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Header } from "./components/";
 import { NotFound, Main } from "./pages";
 import { Routes, Route } from "react-router-dom";
 import "./sass/mian.scss";
-import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+import { BsFillCaretUpFill } from "react-icons/bs";
 const App: FC = (): JSX.Element => {
+  const [webHeight, setwebHeight] = useState(window.scrollY);
+
+  function handle() {
+    setwebHeight(window.scrollY);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handle);
+    return () => {
+      window.removeEventListener("scroll", handle);
+    };
+  }, []);
+
   return (
     <>
       <Header />
@@ -12,12 +27,23 @@ const App: FC = (): JSX.Element => {
         style={{
           marginTop: 70,
           backgroundColor: "black",
+          height: "100%",
         }}
       >
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        {webHeight > 720 && (
+          <Button
+            className="scrollUpBtn d-flex justify-content-center align-items-center"
+            onClick={() => {
+              window.scrollTo({ top: 0 });
+            }}
+          >
+            <BsFillCaretUpFill />
+          </Button>
+        )}
       </div>
     </>
   );
